4a0578dbdeb531b4c441d781a60e702b29ac94bb
Depo90 <davidedepoli90@gmail.com>
2019-07-23 14:51:08 +0200

    Per ogni carburatore genero uno scatterplot

--- Script_prova.R
+++ Script_prova.R
@@ -8,11 +8,13 @@ df = mtcars %>%
 db_1 = read_excel("db_test_git.xlsx")
 
 # plot
-plot = df %>% 
-  ggplot(aes(x = mpg, y = hp, color = carb)) +
-  geom_point() +
-  theme_bw()
-
-# export
-
-ggsave(plot = plot,"scatter.png", width = 7, height = 5)
+for (i in unique(mtcars$carb)){
+  
+  plot = df %>% 
+    ggplot(aes(x = mpg, y = hp, color = am)) +
+    geom_point() +
+    theme_bw()
+  
+  # export
+  ggsave(plot = plot,paste("Carb_",unique(mtcars$carb)[i],"_scatter.png"), width = 7, height = 5)
+}
