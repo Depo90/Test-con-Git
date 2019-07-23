524ccc5c70ac621b5b5c5998429ce1ba389c6971
Depo90 <davidedepoli90@gmail.com>
2019-07-23 14:39:06 +0200

    escludo i 6 cilindri

--- Script_prova.R
+++ Script_prova.R
@@ -3,15 +3,16 @@ library(readxl)
 
 # caricamenti
 df = mtcars %>% 
-  filter(mpg > 20)
+  filter(mpg > 20 & cyl == 6)
 
 db_1 = read_excel("db_test_git.xlsx")
 
 # plot
 plot = df %>% 
   ggplot(aes(x = mpg, y = hp, color = carb)) +
-  geom_point()
+  geom_point() +
+  theme_bw()
 
 # export
 
-ggsave(plot,"scatter.png", width = 7, height = 5)
+ggsave(plot = plot,"scatter.png", width = 7, height = 5)
