eec817afd2121b3a0263acdbe762255173267284
Depo90 <davidedepoli90@gmail.com>
2019-07-23 12:45:16 +0200

    Aggiunta di Scatterplot hp vs mpg

--- Script_prova.R
+++ Script_prova.R
@@ -1,8 +1,13 @@
 library(tidyverse)
 library(readxl)
-mtcars
 
+# caricamenti
 df = mtcars %>% 
   filter(mpg > 20)
 
 db_1 = read_excel("db_test_git.xlsx")
+
+# plot
+df %>% 
+  ggplot(aes(x = mpg, y = hp, color = carb)) +
+  geom_point()
